baae7ed71acfe69ab43f4dcfed6e42c5dd25d563
Amirreza-DigitalCitizen10 <me@amrrza.com>
2024-09-22 14:39:03 +0330

    Edited index.html, Designed Slider, Created our-team.html and fixed some issues.

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+@app.route('/our-team')
+def our_team():
+    return render_template('our-team.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8569)
+    app.run(debug=True, port=5001)
